a3f094f35b3a5257dee13755d602b003a1e12751
ryabintsev <nightout2017@gmail.com>
2024-04-15 17:33:47 +0300

    added fix import button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,12 @@ import { ProviderStack } from '@app/providers/ProviderStack';
 import { Routes } from '@app/routes/Routes';
 import { MainLayout } from '@shared/ui/MainLayout/MainLayout';
 
+import { Aside } from '@widgets/Aside';
 import { Main } from '@widgets/Main';
 import { Sidebar } from '@widgets/Sidebar';
 import { Toolbar } from '@widgets/Toolbar';
 
 import './styles/index.scss';
-import { Aside } from '@widgets/Aside';
 
 export function App() {
   return (
